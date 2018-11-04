c8882abb4a44064d794763a821e00cbb40b68d85
Александр <siretna@gmail.com>
2018-11-04 15:04:59 +0300

    routes & actions

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,10 @@
+class MessagesController < ActionController::API
+  def send_message
+  end
+
+  def send_message_immediately
+  end
+
+  def message_info
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,5 @@
 Rails.application.routes.draw do
+  post 'send',             action: :send_message,             controller: :messages
+  post 'send_immediately', action: :send_message_immediately, controller: :messages
+  get  'info/:message_id', action: :message_info,             controller: :messages
 end
